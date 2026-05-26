5afc8343a44bd3e374a9626dd5364bd5dd62353d
hermit <gkamps@proton.me>
2026-05-26 15:23:21 -0400

    mmmm

--- src/apps.rs
+++ src/apps.rs
@@ -1,5 +1,25 @@
-fn load_apps() {}
+use freedesktop_desktop_entry::{DesktopEntry, desktop_entries};
+use std::{
+    ffi::{OsStr, OsString},
+    path::Path,
+};
 
-fn get_icons(&App_cache: Option<Vec<String>>) {
-    if App_cache != None {}
+fn load_apps() {
+let programs: Vec<program>;
+    let locales_str = freedesktop_desktop_entry::get_languages_from_env();
+    let desktops = desktop_entries(&locales_str);
+    for entry in desktops {
+        if entry.no_display() == true {
+            continue;
+        }
+        if entry.hidden() == true {
+            continue;
+        }
+        programs += program{}
+}
+
+struct program {
+    Name : str,
+    launch_path: OsString,
+    icon: str,
 }
